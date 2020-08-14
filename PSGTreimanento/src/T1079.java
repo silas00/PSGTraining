ed3e1f3d6f662e16a8cedb1397050e2ec103f659
silas00 <silasdasilva.00@gmail.com>
2020-08-14 19:17:02 -0400

    Algoritmo com for

---
+++ PSGTreimanento/src/T1079.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class T1079 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		double media = 0;
+
+		for (int i = 0; i < N; i++) {
+
+			double nota1 = sc.nextDouble();
+			double nota2 = sc.nextDouble();
+			double nota3 = sc.nextDouble();
+
+			media = ((nota1 * 2) + (nota2 * 3) + (nota3 * 5)) / 10;
+
+			System.out.printf("%.1f", media);
+		}
+
+		sc.close();
+	}
+
+}
