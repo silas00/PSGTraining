7701a5cb4f5d79841024c65ba79e9de37e7547b0
silas00 <silasdasilva.00@gmail.com>
2020-08-19 14:49:47 -0400

    Algoitmo de treinamento em java

--- PSGTreimanento/src/main/NOMES.java
+++ PSGTreimanento/src/main/NOMES.java
@@ -8,16 +8,20 @@ public class NOMES {
 
 		Scanner sc = new Scanner(System.in);
 
+		//Parametros do vetor//
 		int n = sc.nextInt();
 		String tempNome;
 		String[] nomes = new String[n];
-
+		
+		//Entrada de dados no vetor//
 		for (int i = 0; i < nomes.length; i++) {
 			nomes[i] = sc.next();
 		}
-
+		
+		//Bubble sort para ordenação de dados dentro do vetor//
 		for (int i = 0; i < nomes.length; i++) {
 			for (int j = 0; j < (nomes.length - 1); j++) {
+				//Swap se verdadeiro para troca de posição dentro do vetor//
 				if (nomes[j].compareTo(nomes[j + 1]) > 0) {
 					tempNome = nomes[j];
 					nomes[j] = nomes[j + 1];
@@ -26,6 +30,7 @@ public class NOMES {
 			}
 		}
 
+		//Saida dos dados já ordenados no vetor//
 		for (int i = 0; i < nomes.length; i++) {
 			System.out.println(nomes[i]);
 		}
