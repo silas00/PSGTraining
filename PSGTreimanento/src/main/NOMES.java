e8fbd77baafe59bd5dcf3648e7bd91711725ce78
silas00 <silasdasilva.00@gmail.com>
2020-08-18 23:03:41 -0400

    Algoritmo de treinamento em java

--- PSGTreimanento/src/main/NOMES.java
+++ PSGTreimanento/src/main/NOMES.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class NOMES {
@@ -10,15 +9,26 @@ public class NOMES {
 		Scanner sc = new Scanner(System.in);
 
 		int n = sc.nextInt();
+		String tempNome;
 		String[] nomes = new String[n];
 
 		for (int i = 0; i < nomes.length; i++) {
 			nomes[i] = sc.next();
 		}
-		
+
+		for (int i = 0; i < nomes.length; i++) {
+			for (int j = 0; j < (nomes.length - 1); j++) {
+				if (nomes[j].compareTo(nomes[j + 1]) > 0) {
+					tempNome = nomes[j];
+					nomes[j] = nomes[j + 1];
+					nomes[j + 1] = tempNome;
+				}
+			}
+		}
+
 		for (int i = 0; i < nomes.length; i++) {
 			System.out.println(nomes[i]);
 		}
-		
+
 	}
 }
