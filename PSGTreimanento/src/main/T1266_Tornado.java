29c3c108bbfe7a44cfada8a38217622041da8554
silas00 <silasdasilva.00@gmail.com>
2020-08-20 18:03:26 -0400

    Desafio do tornado

--- PSGTreimanento/src/main/T1266_Tornado.java
+++ PSGTreimanento/src/main/T1266_Tornado.java
@@ -7,24 +7,34 @@ public class T1266_Tornado {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
-		int [] x = new int[n];
-		int poste = 0;
-		
-		for ( int i = 0; i < x.length; i++) {
-			x[i] = sc.nextInt();
-		}
-		
-		for ( int i = 0; i < (x.length - 1); i++) {
-			if (x[i] == 0 && x[i + 1] == 0) {
-				poste += 1;
-				x[i + 1] = 1;
+
+		while (n != 0) {
+			int[] x = new int[n];
+			int poste = 0;
+
+			for (int i = 0; i < x.length; i++) {
+				x[i] = sc.nextInt();
+			}
+
+			if (x[0] == 0 && x[n - 2] == 1 && x[n -1] == 0) {
+				x[0] = 1;
+				poste++;
+			}
+
+			for (int i = 0; i < (x.length - 1); i++) {
+				if (x[i] == 0 && x[i + 1] == 0) {
+					poste++;
+					x[i + 1] = 1;
 				}
+			}
+
+			System.out.println(poste);
+
+			n = sc.nextInt();
 		}
 
-		System.out.println(poste);
-		
 		sc.close();
 	}
 
